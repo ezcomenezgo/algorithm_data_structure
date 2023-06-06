4cbe368b15323b3b1648324e6a6e026cd847699e
Claire Li <s3310507057@gmail.com>
2023-06-07 00:11:50 +0800

    practice pointer - subsequence

--- pointer.js
+++ pointer.js
@@ -92,3 +92,73 @@ function isPalindrome(str) {
 }
 
 console.log('isPalindrome', isPalindrome('amanaplanacanalpanama'));
+
+function isSubsequence(newStr, originStr) {
+  if (newStr.length === 0) return true;
+
+  let i = 0; // set the index for newStr
+  let j = 0; // set the index for originStr
+
+  // if any of string finished, then end the loop
+  while (i <= newStr.length - 1 && j <= originStr.length - 1) {
+    // original string's index has to be increased every time
+    if (newStr[i] !== originStr[j]) {
+      j++;
+      // new string's index only be increased when paired successfully
+    } else if (newStr[i] === originStr[j]) {
+      i++;
+      j++;
+    }
+  }
+
+  // after while loop
+  if (i === newStr.length) {
+    // if new string's index is at the last position
+    // then represent new string is the subsequence of original string
+    return true;
+  } else if (i !== newStr.length && j === originStr.length) {
+    // if new string's index isn't at the last position but original string is finished
+    // then represent new string is not the subsequence of original string
+    return false;
+  }
+
+  return 'none';
+}
+
+// v2
+function isSubsequence2(newStr, originStr) {
+  if (newStr.length === 0) return true;
+
+  let i = 0;
+  let j = 0;
+
+  while (i <= newStr.length - 1 && j <= originStr.length - 1) {
+    if (newStr[i] === originStr[j]) i++;
+    j++;
+  }
+
+  if (i === newStr.length) return true;
+  else if (i !== newStr.length && j === originStr.length) return false;
+
+  return 'none';
+}
+
+// teacher's solution
+function isSubsequence3(newStr, originStr) {
+  if (newStr.length === 0) return true;
+
+  let i = 0;
+  let j = 0;
+
+  while (j < originStr.length) {
+    if (newStr[i] === originStr[j]) i++;
+
+    if (i >= newStr.length) return true;
+
+    j++;
+  }
+
+  return false;
+}
+
+console.log('isSubsequence', isSubsequence2('abc', 'bca'));
